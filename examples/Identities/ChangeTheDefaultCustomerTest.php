552cb4e0efd5bf873342ba09bb501dc199ba5740
Kevin Schroeder <kschroeder@mirageworks.com>
2016-01-05 16:25:53 -0600

    End of day commit

--- examples/Identities/ChangeTheDefaultCustomerTest.php
+++ examples/Identities/ChangeTheDefaultCustomerTest.php
@@ -4,6 +4,7 @@ namespace Examples\Identities;
 
 use Magium\Magento\AbstractMagentoTestCase;
 
+
 class ChangeTheDefaultCustomerTest extends AbstractMagentoTestCase
 {
     public function testCustomerLogin()
@@ -19,6 +20,43 @@ class Customer extends \Magium\Magento\Identities\Customer
     public function doSomethingAwesome()
     {
         // You can use your imagination
+    }
+
+}
+
+
+
+use Magium\Magento\Themes\Magento19\ThemeConfiguration;
+use Magium\WebDriver\WebDriver;
+
+class MyTheme extends ThemeConfiguration
+{
+
+    public function getSomeId()
+    {
+        return 'some_id';
+    }
+
+}
+
+class MyUsefulNavigator
+{
+
+    protected $webDriver;
+    protected $theme;
+
+    public function __construct(
+        WebDriver $webDriver,
+        MyTheme $theme)
+    {
+        $this->webDriver = $webDriver;
+        $this->theme = $theme;
+    }
+
+
+    public function navigateTo()
+    {
+        $this->webDriver->byId($this->theme->getSomeId())->click();
     }
 
 }
